ebfe5c94cdbf059cb4a391bdaabe0a15f48eb4af
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-08-10 00:14:35 -0400

    created animal prediction

--- backend/model/animal_model.py
+++ backend/model/animal_model.py
@@ -14,3 +14,14 @@ preprocess = transforms.Compose([
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
 ])
 
+def predict_animal(image_path):
+    image = Image.open(image_path)
+    input_tensor = preprocess(image)
+    input_batch = input_tensor.unsqueeze(0)
+
+    with torch.no_grad():
+        output = model(input_batch)
+    
+    _, index = torch.max(output, 1)
+
+    return f'Predicted Class Index: {index.item()}'
