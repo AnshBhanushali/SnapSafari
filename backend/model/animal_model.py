e3adb4b75ccccfdb97fb61d426b0f78e4adc89e4
Ansh Bhanushali <bhanusad@mail.uc.edu>
2024-08-10 00:09:30 -0400

    loading preprocessed model

--- backend/model/animal_model.py
+++ backend/model/animal_model.py
@@ -2,3 +2,15 @@ import torch
 from torchvision import models, transforms
 from PIL import Image
 
+# Load a pre-trained model
+model = models.resnet50(pretrained=True)
+model.eval()
+
+# Define image transformations
+preprocess = transforms.Compose([
+    transforms.Resize(256),
+    transforms.CenterCrop(224),
+    transforms.ToTensor(),
+    transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+
